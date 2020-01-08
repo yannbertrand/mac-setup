bfcbbf2051511ce63cfe7739cade8b36c6b4a069
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-01-08 15:27:20 +0100

    Migrate to ZSH

--- setup.sh
+++ setup.sh
@@ -1,23 +1,32 @@
-echo "# Installing homebrew"
+echo "#----------------------------------------#"
+echo "#             Switch to ZSH              #"
+echo "#----------------------------------------#"
+chsh -s /bin/zsh
+
+echo "#----------------------------------------#"
+echo "#   Installing Apple Command Line Tools  #"
+echo "#----------------------------------------#"
+xcode-select --install
+
+echo "#----------------------------------------#"
+echo "#           Installing Homebrew          #"
+echo "#----------------------------------------#"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "# Installing some utilities"
+echo "#----------------------------------------#"
+echo "#  Installing some utilities using Brew  #"
+echo "#----------------------------------------#"
 brew install mas
 brew install git
 brew install yarn
 brew install openssl
 brew install youtube-dl
+brew install bat
 brew install nvm
 
-echo "# Setting up nvm"
-mkdir ~/.nvm
-echo "export NVM_DIR=\"$HOME/.nvm\"" >> ~/.bash_profile
-echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\" # This loads nvm" >> ~/.bash_profile
-
-echo "# Installing node"
-nvm install --lts # Long-term support
-
-echo "# Installing apps from the Mac App Store"
+echo "#----------------------------------------#"
+echo "# Installing apps from the Mac App Store #"
+echo "#----------------------------------------#"
 mas lucky Gifski
 mas lucky Lungo
 mas lucky "Battery Indicator"
@@ -30,10 +39,9 @@ mas lucky Quiver
 mas lucky "PiPifier - PiP for nearly every video"
 mas lucky Magnet
 
-echo "Installing homebrew cask"
-brew tap homebrew/cask
-
-echo "Installing some casks"
+echo "#----------------------------------------#"
+echo "#       Installing some Brew casks       #"
+echo "#----------------------------------------#"
 brew cask install brave-browser
 brew cask install sublime-text
 brew cask install visual-studio-code
@@ -44,15 +52,42 @@ brew cask install transmission
 brew cask install molotov
 brew cask install cyberduck
 brew cask install bartender
+brew cask install protonmail-bridge
+brew cask install protonvpn
+brew cask install handbrake
+brew cask install docker
+brew cask install gitkraken
+brew cask install iterm2
+brew cask install kap
+brew cask install colorsnapper
 
 if [ -z "$1" ]
   then
-    echo "Setting up your SSH key"
-    echo "What email would you like to use?"
+    echo "#----------------------------------------#"
+    echo "#         Setting up your SSH key        #"
+    echo "#----------------------------------------#"
+    echo "> What email would you like to use?"
     read email
 else
     email=$1
 fi
 
-echo "Generating SSH key"
+echo "# Installing Node.js"
+nvm install node
+
+echo "# Initialising ~/.gitignore"
+echo ".DS_Store\n" >> ~/.gitignore
+
+echo "# Initialising ~/.zshrc"
+echo '#!/bin/zsh' >> ~/.zshrc
+echo '' >> ~/.zshrc
+echo 'export ZSH_CONF="default"' >> ~/.zshrc
+echo 'export PS1="%n:%1~$ "' >> ~/.zshrc
+echo '' >> ~/.zshrc
+echo 'alias cat=bat' >> ~/.zshrc
+echo '' >> ~/.zshrc
+
+echo "#----------------------------------------#"
+echo "#           Generating SSH key           #"
+echo "#----------------------------------------#"
 ssh-keygen -q -t rsa -b 4096 -C $email -f $HOME/.ssh/id_rsa -N ""
