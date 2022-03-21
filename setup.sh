5683356c6f880cf00bec03f64f54911e835aac42
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-03-21 09:52:43 +0100

    Tweak animation settings

--- setup.sh
+++ setup.sh
@@ -25,12 +25,13 @@ echo " Autohide the Dock"
 defaults write com.apple.dock "autohide" -bool "true"
 echo " Remove the Auto-Hide & Show Delay"
 defaults write com.apple.Dock autohide-delay -float 0
-echo " Decrease the Mission Control zoom effect in Mac OS X"
-defaults write com.apple.dock expose-animation-duration -float 0.12
 echo " Keep the Spaces arrangement"
 defaults write com.apple.dock "mru-spaces" -bool "false"
 echo " Relaunch"
 killall Dock
+
+echo " Reduce animations"
+defaults write com.apple.universalaccess reduceMotion -bool true
 
 echo "# Finder"
 echo " Display the file extensions"
